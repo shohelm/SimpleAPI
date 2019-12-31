aca7dee514fd922ef2ff30989758ee577abc6e38
Shohel Mahmud <shohelcse@gmail.com>
2020-01-01 00:55:26 +0100

    Api test value corrected to see the test result

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Shohel1";
+            return "Shohel";
         }
 
         // POST api/values
